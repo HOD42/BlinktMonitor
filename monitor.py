e3f048e926d35a6d0bad5a14e2130028a3895613
Ian Hodgetts <HOD42@users.noreply.github.com>
2018-07-20 18:08:33 +0100

    Abstraction and configuration of hostnames
    Removing the specific queries for hostnames and moving to configurable queries (at least the hostnames).

--- monitor.py
+++ monitor.py
@@ -12,6 +12,14 @@ account = config.get('NewRelic', 'account')
 queryKey = config.get('NewRelic', 'queryKey')
 queryTimeout = float(config.get('NewRelic', 'queryTimeout'))
 hostnames = config.get('Hosts', 'hostnames')
+pixel0Host = config.get('Hosts', 'pixel0Host')
+pixel1Host = config.get('Hosts', 'pixel1Host')
+pixel2Host = config.get('Hosts', 'pixel2Host')
+pixel3Host = config.get('Hosts', 'pixel3Host')
+pixel4Host = config.get('Hosts', 'pixel4Host')
+pixel5Host = config.get('Hosts', 'pixel5Host')
+pixel6Host = config.get('Hosts', 'pixel6Host')
+pixel7Host = config.get('Hosts', 'pixel7Host')
 pollDelay = int(config.get('Delays', 'pollDelay'))
 net_scale = int(config.get('Scales', 'net_scale'))
 global_bright = float(config.get('Brightness', 'global_bright'))
@@ -22,6 +30,8 @@ cpu_error_value = float(config.get('Brightness', 'cpu_error_value'))/cpu_bright
 disk_error_value = float(config.get('Brightness', 'disk_error_value'))/disk_bright
 net_error_value = float(config.get('Brightness', 'net_error_value'))/net_bright
 
+# q1 = red, q2 = blue, q3 = green ?
+
 while True:
     # 1st query (for CPU)
     raw_nrql="SELECT average(cpuPercent) FROM SystemSample FACET hostname WHERE hostname LIKE '"+hostnames+"%' SINCE 1 minute ago"
@@ -31,20 +41,70 @@ while True:
     q1.get_response(queryTimeout)
     if q1.json_response == None:
         print("No q1")
-        cpu_web01=cpu_web04=cpu_web02=cpu_app01=cpu_app02=cpu_sql01=cpu_error_value
+        q1pixel1=q1pixel2=q1pixel3=q1pixel4=q1pixel5=q1pixel6=cpu_error_value
     else:
-        cpu_web01=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB01')
-        print("cpu_web01="+str(cpu_web01))
-        cpu_web04=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB04')
-        print("cpu_web04="+str(cpu_web04))
-        cpu_web02=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB02')
-        print("cpu_web02="+str(cpu_web02))
-        cpu_app01=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTAPP01')
-        print("cpu_app01="+str(cpu_app01))    
-        cpu_app02=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTAPP02')
-        print("cpu_app02="+str(cpu_app02))   
-        cpu_sql01=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTSQL01')
-        print("cpu_sql01="+str(cpu_sql01))
+        # Trial for integer to override query
+        try:
+            int(pixel0Host)
+            q1pixel0=float(pixel0Host)
+        except:
+            q1pixel0=ExtractValue.get_average(q1.json_response, pixel0Host)
+        print("q1pixel0 ("+pixel0Host+")="+str(q1pixel0))
+        
+        #q1pixel1=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB01')    
+        try:
+            int(pixel1Host)
+            q1pixel1=float(pixel1Host)
+        except:
+            q1pixel1=ExtractValue.get_average(q1.json_response, pixel1Host)
+        print("q1pixel1 ("+pixel1Host+")="+str(q1pixel1))
+        
+        #q1pixel2=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB04')
+        try:
+            int(pixel2Host)
+            q1pixel2=float(pixel2Host)
+        except:
+            q1pixel2=ExtractValue.get_average(q1.json_response, pixel2Host)
+        print("q1pixel2 ("+pixel2Host+")="+str(q1pixel2))
+        
+        #q1pixel3=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTWEB02')
+        try:
+            int(pixel3Host)
+            q1pixel3=float(pixel3Host)
+        except:
+            q1pixel3=ExtractValue.get_average(q1.json_response, pixel3Host)
+        print("q1pixel3 ("+pixel3Host+")="+str(q1pixel3))
+        
+        #q1pixel4=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTAPP01')
+        try:
+            int(pixel4Host)
+            q1pixel4=float(pixel4Host)
+        except:
+            q1pixel4=ExtractValue.get_average(q1.json_response, pixel4Host)
+        print("q1pixel4 ("+pixel4Host+")="+str(q1pixel4))
+        
+        #q1pixel5=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTAPP02')
+        try:
+            int(pixel5Host)
+            q1pixel5=float(pixel5Host)
+        except:
+            q1pixel5=ExtractValue.get_average(q1.json_response, pixel5Host)
+        print("q1pixel5 ("+pixel5Host+")="+str(q1pixel5))
+        
+        #q1pixel6=ExtractValue.get_average(q1.json_response, 'OLDCSSTESTSQL01')
+        try:
+            int(pixel6Host)
+            q1pixel6=float(pixel6Host)
+        except:
+            q1pixel6=ExtractValue.get_average(q1.json_response, pixel6Host)
+        print("q1pixel6 ("+pixel6Host+")="+str(q1pixel6))
+
+        try:
+            int(pixel7Host)
+            q1pixel7=float(pixel7Host)
+        except:
+            q1pixel7=ExtractValue.get_average(q1.json_response, pixel7Host)
+        print("q1pixel7 ("+pixel7Host+")="+str(q1pixel7))
 
     # 2nd query (for disk)    
     raw_nrql="SELECT average(totalUtilizationPercent) FROM StorageSample FACET hostname WHERE hostname LIKE '"+hostnames+"%' SINCE 1 minute ago"
@@ -54,20 +114,70 @@ while True:
     q2.get_response(queryTimeout)
     if q2.json_response == None:
         print("No q2")
-        disk_web01=disk_web04=disk_web02=disk_app01=disk_app02=disk_sql01=disk_error_value
+        q2pixel1=q2pixel2=q2pixel3=q2pixel4=q2pixel5=q2pixel6=disk_error_value
     else:   
-        disk_web01=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB01')
-        print("disk_web01="+str(disk_web01))
-        disk_web04=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB04')
-        print("disk_web04="+str(disk_web04))
-        disk_web02=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB02')
-        print("disk_web02="+str(disk_web02))
-        disk_app01=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTAPP01')
-        print("disk_app01="+str(disk_app01))
-        disk_app02=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTAPP02')
-        print("disk_app02="+str(disk_app02))
-        disk_sql01=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTSQL01')
-        print("disk_sql01="+str(disk_sql01))
+        # Trial for integer to override query
+        try:
+            int(pixel0Host)
+            q2pixel0=float(pixel0Host)
+        except:
+            q2pixel0=ExtractValue.get_average(q2.json_response, pixel0Host)
+        print("q2pixel0 ("+pixel0Host+")="+str(q2pixel0))
+            
+        #q2pixel1=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB01')    
+        try:
+            int(pixel1Host)
+            q2pixel1=float(pixel1Host)
+        except:
+            q2pixel1=ExtractValue.get_average(q2.json_response, pixel1Host)
+        print("q2pixel1 ("+pixel1Host+")="+str(q2pixel1))
+        
+        #q2pixel2=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB04')
+        try:
+            int(pixel2Host)
+            q2pixel2=float(pixel2Host)
+        except:
+            q2pixel2=ExtractValue.get_average(q2.json_response, pixel2Host)
+        print("q2pixel2 ("+pixel2Host+")="+str(q2pixel2))
+        
+        #q2pixel3=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTWEB02')
+        try:
+            int(pixel3Host)
+            q2pixel3=float(pixel3Host)
+        except:
+            q2pixel3=ExtractValue.get_average(q2.json_response, pixel3Host)
+        print("q2pixel3 ("+pixel3Host+")="+str(q2pixel3))
+        
+        #q2pixel4=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTAPP01')
+        try:
+            int(pixel4Host)
+            q2pixel4=float(pixel4Host)
+        except:
+            q2pixel4=ExtractValue.get_average(q2.json_response, pixel4Host)
+        print("q2pixel4 ("+pixel4Host+")="+str(q2pixel4))
+        
+        #q2pixel5=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTAPP02')
+        try:
+            int(pixel5Host)
+            q2pixel5=float(pixel5Host)
+        except:
+            q2pixel5=ExtractValue.get_average(q2.json_response, pixel5Host)
+        print("q2pixel5 ("+pixel5Host+")="+str(q2pixel5))
+        
+        #q2pixel6=ExtractValue.get_average(q2.json_response, 'OLDCSSTESTSQL01')
+        try:
+            int(pixel6Host)
+            q2pixel6=float(pixel6Host)
+        except:
+            q2pixel6=ExtractValue.get_average(q2.json_response, pixel6Host)
+        print("q2pixel6 ("+pixel6Host+")="+str(q2pixel6))
+
+        try:
+            int(pixel7Host)
+            q2pixel7=float(pixel7Host)
+        except:
+            q2pixel7=ExtractValue.get_average(q2.json_response, pixel7Host)
+        print("q2pixel7 ("+pixel7Host+")="+str(q2pixel7))
 
     # 3rd query (for network receive and transmit = net_tot)
     raw_nrql="SELECT average(receiveBytesPerSecond) + average(transmitBytesPerSecond) as 'net_tot' FROM NetworkSample FACET hostname WHERE hostname LIKE '"+hostnames+"%' SINCE 1 minute ago"
@@ -78,56 +188,108 @@ while True:
     q3.get_response(queryTimeout)
     if q3.json_response == None:
         print("No q3")
-        net_tot_web01=net_tot_web04=net_tot_web02=net_tot_app01=net_tot_app02=net_tot_sql01=net_error_value
+        q3pixel1=q3pixel2=q3pixel3=q3pixel4=q3pixel5=q3pixel6=net_error_value
     else:  
-        net_tot_web01=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB01')
-        print("net_tot_web01="+str(net_tot_web01))
-        net_tot_web04=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB04')
-        print("net_tot_web04="+str(net_tot_web04))
-        net_tot_web02=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB02')
-        print("net_tot_web02="+str(net_tot_web02))
-        net_tot_app01=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTAPP01')
-        print("net_tot_app01="+str(net_tot_app01))
-        net_tot_app02=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTAPP02')
-        print("net_tot_app02="+str(net_tot_app02))
-        net_tot_sql01=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTSQL01')
-        print("net_tot_sql01="+str(net_tot_sql01))
+        # Trial for integer to override query
+        try:
+            int(pixel0Host)
+            q3pixel0=float(pixel0Host)
+        except:
+            q3pixel0=ExtractValue.get_result(q3.json_response, pixel0Host)
+        print("q3pixel0 ("+pixel0Host+")="+str(q3pixel0))
+            
+        #q3pixel1=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB01')    
+        try:
+            int(pixel1Host)
+            q3pixel1=float(pixel1Host)
+        except:
+            q3pixel1=ExtractValue.get_result(q3.json_response, pixel1Host)
+        print("q3pixel1 ("+pixel1Host+")="+str(q3pixel1))
+        
+        #q3pixel2=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB04')
+        try:
+            int(pixel2Host)
+            q3pixel2=float(pixel2Host)
+        except:
+            q3pixel2=ExtractValue.get_result(q3.json_response, pixel2Host)
+        print("q3pixel2 ("+pixel2Host+")="+str(q3pixel2))
+        
+        #q3pixel3=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTWEB02')
+        try:
+            int(pixel3Host)
+            q3pixel3=float(pixel3Host)
+        except:
+            q3pixel3=ExtractValue.get_result(q3.json_response, pixel3Host)
+        print("q3pixel3 ("+pixel3Host+")="+str(q3pixel3))
+        
+        #q3pixel4=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTAPP01')
+        try:
+            int(pixel4Host)
+            q3pixel4=float(pixel4Host)
+        except:
+            q3pixel4=ExtractValue.get_result(q3.json_response, pixel4Host)
+        print("q3pixel4 ("+pixel4Host+")="+str(q3pixel4))
+        
+        #q3pixel5=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTAPP02')
+        try:
+            int(pixel5Host)
+            q3pixel5=float(pixel5Host)
+        except:
+            q3pixel5=ExtractValue.get_result(q3.json_response, pixel5Host)
+        print("q3pixel5 ("+pixel5Host+")="+str(q3pixel5))
+        
+        #q3pixel6=ExtractValue.get_result(q3.json_response, 'OLDCSSTESTSQL01')
+        try:
+            int(pixel6Host)
+            q3pixel6=float(pixel6Host)
+        except:
+            q3pixel6=ExtractValue.get_result(q3.json_response, pixel6Host)
+        print("q3pixel6 ("+pixel6Host+")="+str(q3pixel6))
+
+        try:
+            int(pixel7Host)
+            q3pixel7=float(pixel7Host)
+        except:
+            q3pixel7=ExtractValue.get_result(q3.json_response, pixel7Host)
+        print("q3pixel7 ("+pixel7Host+")="+str(q3pixel7))
 
     # Process query results (scale/brightness)
     
-    cpu_web01=cpu_web01*cpu_bright*global_bright
-    disk_web01=disk_web01*disk_bright*global_bright
-    net_tot_web01=(net_tot_web01/net_scale)*net_bright*global_bright
+    q1pixel1=q1pixel1*cpu_bright*global_bright
+    q2pixel1=q2pixel1*disk_bright*global_bright
+    q3pixel1=(q3pixel1/net_scale)*net_bright*global_bright
 
-    cpu_web04=cpu_web04*cpu_bright*global_bright
-    disk_web04=disk_web04*disk_bright*global_bright
-    net_tot_web04=(net_tot_web04/net_scale)*net_bright*global_bright
+    q1pixel2=q1pixel2*cpu_bright*global_bright
+    q2pixel2=q2pixel2*disk_bright*global_bright
+    q3pixel2=(q3pixel2/net_scale)*net_bright*global_bright
 
-    cpu_web02=cpu_web02*cpu_bright*global_bright
-    disk_web02=disk_web02*disk_bright*global_bright
-    net_tot_web02=(net_tot_web02/net_scale)*net_bright*global_bright
+    q1pixel3=q1pixel3*cpu_bright*global_bright
+    q2pixel3=q2pixel3*disk_bright*global_bright
+    q3pixel3=(q3pixel3/net_scale)*net_bright*global_bright
 
-    cpu_app01=cpu_app01*cpu_bright*global_bright
-    disk_app01=disk_app01*disk_bright*global_bright
-    net_tot_app01=(net_tot_app01/net_scale)*net_bright*global_bright
+    q1pixel4=q1pixel4*cpu_bright*global_bright
+    q2pixel4=q2pixel4*disk_bright*global_bright
+    q3pixel4=(q3pixel4/net_scale)*net_bright*global_bright
 
-    cpu_app02=cpu_app02*cpu_bright*global_bright
-    disk_app02=disk_app02*disk_bright*global_bright
-    net_tot_app02=(net_tot_app02/net_scale)*net_bright*global_bright
+    q1pixel5=q1pixel5*cpu_bright*global_bright
+    q2pixel5=q2pixel5*disk_bright*global_bright
+    q3pixel5=(q3pixel5/net_scale)*net_bright*global_bright
 
-    cpu_sql01=cpu_sql01*cpu_bright*global_bright
-    disk_sql01=disk_sql01*disk_bright*global_bright
-    net_tot_sql01=(net_tot_sql01/net_scale)*net_bright*global_bright
+    q1pixel6=q1pixel6*cpu_bright*global_bright
+    q2pixel6=q2pixel6*disk_bright*global_bright
+    q3pixel6=(q3pixel6/net_scale)*net_bright*global_bright
 
 # Modify output for BLINKT here.
-    OutputToBlinkt.set_values(1,1,1,
-                             cpu_web01,net_tot_web01,disk_web01,
-                             cpu_web04,net_tot_web04,disk_web04,
-                             cpu_web02,net_tot_web02,disk_web02,
-                             cpu_app01,net_tot_app01,disk_app01,
-                             cpu_app02,net_tot_app02,disk_app02,
-                             cpu_sql01,net_tot_sql01,disk_sql01,
-                             25*global_bright,25*global_bright,25*global_bright)
+    OutputToBlinkt.set_values(
+                             q1pixel0,q3pixel0,q2pixel0,
+                             q1pixel1,q3pixel1,q2pixel1,
+                             q1pixel2,q3pixel2,q2pixel2,
+                             q1pixel3,q3pixel3,q2pixel3,
+                             q1pixel4,q3pixel4,q2pixel4,
+                             q1pixel5,q3pixel5,q2pixel5,
+                             q1pixel6,q3pixel6,q2pixel6,
+                             q1pixel7,q3pixel7,q2pixel7,
+                             )
                              
     time.sleep(pollDelay)
 
